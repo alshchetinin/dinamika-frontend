2dafca9445b84c1fa3e8cdd5cf88fdb84e6315bd
alshchetinin <alexsander.qw@gmail.com>
2023-06-22 11:01:35 +0800

    feat: Добавляет анимацию

--- components/Section/SectionHero.vue
+++ components/Section/SectionHero.vue
@@ -1,12 +1,15 @@
 <template>
   <section class="h-screen flex flex-col justify-end py-16 relative">
     <div class="container z-10">
-      <h1 class="text-lg lg:text-2xl font-semibold uppercase text-white mb-5">
+      <h1
+        ref="element"
+        class="text-lg lg:text-2xl font-semibold uppercase text-white mb-5"
+      >
         динамика – Команда, объединившая ведущих производителей
         кабелЬНО-ПРОВОДНИКОВОЙ, кабеленесущих систем и эстакад для реализации
         сложных электротехнических проектов в нефтегазовой сфере
       </h1>
-      <ui-button text="Получить консультацию" size="medium" variant="primary" />
+      <!-- <ui-button text="Получить консультацию" size="medium" variant="primary -->
     </div>
     <div class="absolute inset-0">
       <img
@@ -18,6 +21,34 @@
   </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SplitType from 'split-type'
+import { gsap } from 'gsap'
+const element = ref(null)
+
+onMounted(() => {
+  const splitTitle = new SplitType(element.value, {
+    types: 'lines, words',
+  })
+  const splitTitleWords = splitTitle.words
+  gsap.fromTo(
+    splitTitleWords,
+    {
+      opacity: 0,
+      y: 100,
+    },
+    {
+      opacity: 1,
+      y: 0,
+      duration: 0.8,
+      stagger: {
+        each: 0.02,
+        from: 'start',
+        ease: 'power2.inOut',
+      },
+    }
+  )
+})
+</script>
 
 <style></style>
