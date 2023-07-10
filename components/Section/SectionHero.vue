b64bf1dd04febe0afe46b9e5c95ebe29413ba935
alshchetinin <alexsander.qw@gmail.com>
2023-07-10 10:24:34 +0800

    feat: Добавялет блоки

--- components/Section/SectionHero.vue
+++ components/Section/SectionHero.vue
@@ -3,23 +3,25 @@
     <div class="container z-10 text-white">
       <h1
         ref="element"
-        class="text-base lg:text-2xl font-semibold uppercase mb-5"
+        class="animation-title text-base lg:text-3xl font-semibold uppercase mb-5"
       >
         динамика – Команда, объединившая ведущих производителей
         кабелЬНО-ПРОВОДНИКОВОЙ продукции, кабеленесущих систем и эстакад для
         реализации сложных электротехнических проектов в нефтегазовой сфере
       </h1>
-      <p class="mb-4">
+      <p ref="text" class="mb-4">
         Над нашим сайтом ведутся работы, он скоро заработает в новом формате.
         <br />
         А пока оставьте ваш номер и мы свяжемся с вами
       </p>
-      <ui-button
-        text="Оставить заявку"
-        size="large"
-        variant="primary"
-        @click="open"
-      />
+      <div ref="button">
+        <ui-button
+          text="Оставить заявку"
+          size="large"
+          variant="primary"
+          @click="open"
+        />
+      </div>
     </div>
     <div class="absolute inset-0">
       <!-- <nuxt-img
@@ -43,7 +45,7 @@
 
 <script setup lang="ts">
 import SplitType from 'split-type'
-import { gsap } from 'gsap'
+import { animate, stagger, spring } from 'motion'
 import { useModal } from 'vue-final-modal'
 import ModalForm from '../Modal/ModalForm.vue'
 
@@ -56,29 +58,40 @@ const { open, close } = useModal({
   },
 })
 
-const element = ref(null)
+const element = ref<HTMLElement | null>(null)
+const text = ref<HTMLElement | null>(null)
+const button = ref<HTMLElement | null>(null)
+
 onMounted(() => {
-  const splitTitle = new SplitType(element.value, {
-    types: 'lines, words',
+  const splitTitle = new SplitType('.animation-title', {
+    types: 'words',
   })
-  const splitTitleWords = splitTitle.words
-  gsap.fromTo(
-    splitTitleWords,
-    {
-      opacity: 0,
-      y: 100,
-    },
-    {
-      opacity: 1,
-      y: 0,
-      duration: 0.8,
-      stagger: {
-        each: 0.02,
-        from: 'start',
-        ease: 'power2.inOut',
+
+  let anmationArray: (HTMLElement | null)[] = splitTitle.words || []
+
+  if (text.value) {
+    anmationArray.push(text.value)
+  }
+  if (button.value) {
+    anmationArray.push(button.value)
+  }
+
+  anmationArray = anmationArray.filter((el): el is HTMLElement => el !== null)
+
+  if (anmationArray.length > 0) {
+    animate(
+      anmationArray as HTMLElement[],
+      {
+        opacity: [0, 1],
+        y: [40, 0],
       },
-    }
-  )
+      {
+        easing: spring(),
+        duration: 1.5,
+        delay: stagger(0.03),
+      }
+    )
+  }
 })
 </script>
 
