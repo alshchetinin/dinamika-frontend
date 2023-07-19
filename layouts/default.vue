83c672f72ef3513077188f3b8e48d82b5605f025
alshchetinin <alexsander.qw@gmail.com>
2023-07-19 08:40:45 +0700

    fix: 404 ошибка при сборке

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <SeoKit />
-  <OgImageStatic />
+  <OgImageScreenshot />
   <AppHeader />
   <main class="font-sans">
     <slot />
---
+++ pages/policy.vue
@@ -0,0 +1,13 @@
+<template>
+  <SectionHero
+    title="Политика конфиденциальности"
+    video=""
+    image="contact.jpg"
+  />
+</template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Политика конфиденциальности',
+})
+</script>
