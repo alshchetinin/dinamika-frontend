4e10b27ea3f43e9e1641e659d32d3dc7a62e7322
alshchetinin <alexsander.qw@gmail.com>
2023-07-21 11:10:42 +0700

    fix: Убирает анимацию карточек на телефоне

--- components/Section/SectionAdvantages.vue
+++ components/Section/SectionAdvantages.vue
@@ -14,33 +14,36 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 const { getAdvantage } = useData()
 const { data: advantages } = await getAdvantage()
 onMounted(() => {
-  gsap.registerPlugin(ScrollTrigger)
-  const cards = gsap.utils.toArray('.card')
+  //if mobile
+  if (window.innerWidth > 768) {
+    gsap.registerPlugin(ScrollTrigger)
+    const cards = gsap.utils.toArray('.card')
 
-  cards.forEach((card, index) => {
-    const tween = gsap.to(card, {
-      scrollTrigger: {
+    cards.forEach((card, index) => {
+      const tween = gsap.to(card, {
+        scrollTrigger: {
+          trigger: card,
+          start: () => `top bottom-=100`,
+          end: () => `top top+=40`,
+          scrub: true,
+          // markers: true,
+          invalidateOnRefresh: true,
+        },
+        ease: 'none',
+        // scale: () => 1 - (cards.length - index) * 0.025,
+      })
+
+      ScrollTrigger.create({
         trigger: card,
-        start: () => `top bottom-=100`,
-        end: () => `top top+=40`,
-        scrub: true,
+        start: 'top top',
+        pin: true,
+        pinSpacing: false,
         // markers: true,
+        id: 'pin',
+        end: 'max',
         invalidateOnRefresh: true,
-      },
-      ease: 'none',
-      // scale: () => 1 - (cards.length - index) * 0.025,
+      })
     })
-
-    ScrollTrigger.create({
-      trigger: card,
-      start: 'top top',
-      pin: true,
-      pinSpacing: false,
-      // markers: true,
-      id: 'pin',
-      end: 'max',
-      invalidateOnRefresh: true,
-    })
-  })
+  }
 })
 </script>
